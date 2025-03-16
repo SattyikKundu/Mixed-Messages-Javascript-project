82c4696d45a735069f43a64bd76dfe5a6e2a73f7
Sattyik Kundu <sattyikkundu@gmail.com>
2025-03-16 19:30:21 -0400

    Created advice-arrays file and work_place advice array
    Created a Javascript file that stores the different arrays for random advice. Currently, I finished the workplace_advice array

---
+++ advice-arrays.js
@@ -0,0 +1,23 @@
+
+// workplace advice array with 20 advices
+const workplace_advice = [
+    "Use tools like to-do lists and calendars to manage your tasks efficiently",
+    "Ensure your messages can be understood by all parties by being clear and consise",
+    "Arrive on time for meetings and appointments to show respect",
+    "Set goals and objectives to guide your work and measure progress",
+    "Actively listen and pay attention to what others say to improve collaboration",
+    "Maintain a positive attitude even in challenging situations",
+    "Learn from your mistakes and use them as opportunities to grow and improve",
+    "Always be open to changes and new ideas in your workplace",
+    "Express your gratitude to colleagues for their help and support",
+    "Taking regular breaks can boost productivity and reduce stress",
+    "Drink plenty of water throughout the day to stay focused",
+    "Share appropriate jokes to lighten the mood and reduce tension",
+    "Treat colleagues with kindness and respect",
+    "Prioritize the most important tasks first to maximize efficiency",
+    "Don't hesitate to ask for feedback to improve your performance and skills",
+    "Be Proactive: Anticipate challenges and take initiative to solve them",
+    "Use Technology Wisely: Leverage tools to streamline work processes and save time",
+    "Celebrate Successes: Acknowledge and celebrate achievements to boost morale",
+    "Maintain a healthy balance between your work life and personal life",
+    "ontinuously seek knowledge and skills to enhance your professional growth"];
