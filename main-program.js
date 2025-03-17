db5370861bcaa7b3e181e6baeccc1e706d039d34
Sattyik Kundu <sattyikkundu@gmail.com>
2025-03-16 20:35:22 -0400

    Finished first draft of main-program.js
    Despite finishing first draft of main-program.js, there are issues to work out

--- main-program.js
+++ main-program.js
@@ -1,4 +1,4 @@
-import {workplace_advice, financial_advice, health_advice} from './advice-arrays.js';
+const {workplace_advice, financial_advice, health_advice} = require('./advice-arrays.js');
 
 // Function to return a random Index for an array
 const getRandIndex = (array) => {
@@ -11,3 +11,35 @@ let randIndex_workplace = getRandIndex(workplace_advice);
 let randIndex_financial = getRandIndex(financial_advice);
 let randIndex_health    = getRandIndex(health_advice);
 
+// 3 random advices object
+let random_advices = {
+    workplace_advice: workplace_advice[randIndex_workplace],
+    financial_advice: financial_advice[randIndex_financial],
+    health_advice:    health_advice[randIndex_health]
+};
+
+// Array to store final output to print
+let output = [];
+
+// Store all 3 random advice output string into output array via for-loop
+for (advice in random_advices) {
+    switch(advice) {
+        case 'workplace_advice':
+            output.push(`Workplace advice 🏢: ${random_advices[workplace_advice]}`);
+        case 'financial_advice':
+            output.push(`Financial advice 💰: ${random_advices[financial_advice]}`);
+        case 'health_advice':
+            output.push(`Health advice 🥗:    ${random_advices[health_advice]}`);
+        default:
+            output.push('One or more advice quotes is missing...?');
+    }
+}
+
+// function to format and print 3 random advices stored in output
+const formatAndPrint = (output_array) => {
+    const formattedOuput = output_array.join('\n');
+    console.log(formattedOuput);
+}
+
+// Finally, print 3 random advice
+formatAndPrint(output);
