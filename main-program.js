c2a09f53f6d027595892651deee5acaf386eff78
Sattyik Kundu <sattyikkundu@gmail.com>
2025-03-16 20:11:03 -0400

    Added imports and function to get randomIndex
    Added fuction to generate randomIndex value from imported array. Then generated 3 random Index numbers.

--- main-program.js
+++ main-program.js
@@ -1,1 +1,13 @@
 import {workplace_advice, financial_advice, health_advice} from './advice-arrays.js';
+
+// Function to return a random Index for an array
+const getRandIndex = (array) => {
+    let randIndex = Math.floor(Math.random() * array.length);
+    return randIndex;
+}
+
+// Generate 3 random Indexes
+let randIndex_workplace = getRandIndex(workplace_advice);
+let randIndex_financial = getRandIndex(financial_advice);
+let randIndex_health    = getRandIndex(health_advice);
+
